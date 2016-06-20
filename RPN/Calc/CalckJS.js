2f58e680ce64f16a53c91add84998589e5d38dd2
Beshenka <narkomant000@gmail.com>
2016-06-20 20:19:21 +0300

    New ver

--- RPN/Calc/CalckJS.js
+++ RPN/Calc/CalckJS.js
@@ -1,7 +1,80 @@
+function inpitExp() {
+    var expression = document.querySelectorAll('#calculator span');
+    var operators = ['+', '-', 'x', '/', '^', '(', ')'];
+    var decimalAdded = false;
 
+
+for(var i = 0; i < expression.length; i++) {
+	expression[i].onclick = function(e) {
+        
+		var input = document.querySelector('.screen');
+		var inputVal = input.innerHTML;
+		var btnVal = this.innerHTML;
+		
+
+		if(btnVal == 'C') {
+			input.innerHTML = '';
+			decimalAdded = false;
+		}
+		
+		else if(btnVal == '=') {
+			var equation = inputVal;
+			var lastChar = equation[equation.length - 1];
+			
+
+			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+			
+			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+				equation = equation.replace(/.$/, '');
+			
+			if(equation)
+				input.innerHTML = eval(equation);
+				
+			decimalAdded = false;
+		}
+		
+
+		else if(operators.indexOf(btnVal) > -1) {
+
+			var lastChar = inputVal[inputVal.length - 1];
+			
+			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+				input.innerHTML += btnVal;
+			
+			else if(inputVal == '' && btnVal == '-') 
+				input.innerHTML += btnVal;
+			
+			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+
+				input.innerHTML = inputVal.replace(/.$/, btnVal);
+			}
+			
+			decimalAdded =false;
+		}
+		
+		else if(btnVal == '.') {
+			if(!decimalAdded) {
+				input.innerHTML += btnVal;
+				decimalAdded = true;
+			}
+		}
+		
+		else {
+			input.innerHTML += btnVal;
+		}
+		
+		e.preventDefault();
+	} 
+}
+    
+    
+    
+    
+}
 
 function rpn(expression) {
     "use strict";
+    
     
     var stack = [],
         str = '',
@@ -51,8 +124,7 @@ function rpn(expression) {
     while(t = stack.pop || t!= undefined)
         str = str + ' ' + t;
    
-}
-    
+}    
 
 function get(k) {
 return priority[k];
@@ -130,4 +202,3 @@ function sdvig(str, i){
          
 }
 
-console.log(rpn("2+2"));
